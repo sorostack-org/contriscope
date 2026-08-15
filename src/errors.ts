a100696e8ea59db5778de13c7819419ca77b7e18
Tobi Olusanya <developer@orbitchain.io>
2026-08-15 12:45:05 +0100

    feat(errors): add typed error classes

---
+++ src/errors.ts
@@ -0,0 +1,44 @@
+export class ContriscopeError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "ContriscopeError";
+  }
+}
+
+export class ConfigError extends ContriscopeError {
+  constructor(message: string) {
+    super(message);
+    this.name = "ConfigError";
+  }
+}
+
+export class UsageError extends ContriscopeError {
+  constructor(message: string) {
+    super(message);
+    this.name = "UsageError";
+  }
+}
+
+export class InputError extends ContriscopeError {
+  constructor(message: string) {
+    super(message);
+    this.name = "InputError";
+  }
+}
+
+export interface GitHubErrorOptions {
+  status?: number;
+  rateLimited?: boolean;
+}
+
+export class GitHubError extends ContriscopeError {
+  status?: number;
+  rateLimited: boolean;
+
+  constructor(message: string, options: GitHubErrorOptions = {}) {
+    super(message);
+    this.name = "GitHubError";
+    this.status = options.status;
+    this.rateLimited = options.rateLimited ?? false;
+  }
+}
